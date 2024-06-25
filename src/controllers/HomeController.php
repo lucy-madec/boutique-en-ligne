c4ef8c8d2955faaac63bbf7255120c86957dd703
lucas-iribaren <lucas.iribaren@laplateforme.io>
2024-06-25 14:15:57 +0200

    ajout route home controller

---
+++ src/controllers/HomeController.php
@@ -0,0 +1,9 @@
+// src/controllers/HomeController.php
+<?php
+require_once 'config/db.php';
+require_once 'src/models/Produit.php';
+
+$produit = new Produit($pdo);
+$produitsPhares = $produit->getProduitsPhares();
+
+require 'src/views/home.php';
