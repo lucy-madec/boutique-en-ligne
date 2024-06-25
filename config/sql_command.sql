0b6f9ad6fad6001a0fe13abc8e1f163d726a7196
lucas-iribaren <lucas.iribaren@laplateforme.io>
2024-06-25 19:03:29 +0200

    ajout des tables sql

---
+++ config/sql_command.sql
@@ -0,0 +1,70 @@
+CREATE TABLE `pixel_plush`.`utilisateur` (
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `nom` VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+    `email` VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+    `mot_de passe` VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+    `date_inscription` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (`id`),
+    UNIQUE (`email`)
+) ENGINE = InnoDB;
+
+CREATE TABLE `pixel_plush`.`categorie` (
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `nom` VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB;
+
+CREATE TABLE `pixel_plush`.`commande` (
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `utilisateur_id` INT NOT NULL,
+    `date_commande` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`utilisateur_id`) REFERENCES `utilisateur`(`id`)
+) ENGINE = InnoDB;
+
+CREATE TABLE `pixel_plush`.`sous_categorie` (
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `nom` VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+    `categorie_id` INT NOT NULL,
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`categorie_id`) REFERENCES `categorie`(`id`)
+) ENGINE = InnoDB;
+
+CREATE TABLE `pixel_plush`.`produit` (
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `nom` VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+    `description` TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+    `prix` DECIMAL(10, 2) NOT NULL,
+    `sous_categorie_id` INT NOT NULL,
+    `phare` BOOLEAN NOT NULL DEFAULT 0,
+    `date_ajout` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`sous_categorie_id`) REFERENCES `sous_categorie`(`id`)
+) ENGINE = InnoDB;
+
+
+CREATE TABLE `pixel_plush`.`panier` (
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `utilisateur_id` INT NOT NULL,
+    `date_creation` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`utilisateur_id`) REFERENCES `utilisateur`(`id`)
+) ENGINE = InnoDB;
+
+CREATE TABLE `pixel_plush`.`panier_produit` (
+    `panier_id` INT NOT NULL,
+    `produit_id` INT NOT NULL,
+    `quantite` INT NOT NULL,
+    PRIMARY KEY (`panier_id`, `produit_id`),
+    FOREIGN KEY (`panier_id`) REFERENCES `panier`(`id`),
+    FOREIGN KEY (`produit_id`) REFERENCES `produit`(`id`)
+) ENGINE = InnoDB;
+
+CREATE TABLE `pixel_plush`.`commande_produit` (
+    `commande_id` INT NOT NULL,
+    `produit_id` INT NOT NULL,
+    `quantite` INT NOT NULL,
+    PRIMARY KEY (`commande_id`, `produit_id`),
+    FOREIGN KEY (`commande_id`) REFERENCES `commande`(`id`),
+    FOREIGN KEY (`produit_id`) REFERENCES `produit`(`id`)
+) ENGINE = InnoDB;
