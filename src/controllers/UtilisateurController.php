97e312ef63dfe3d6ea688d5565f9d584f6a5b2e4
lucy-madec <144667458+lucy-madec@users.noreply.github.com>
2024-06-26 13:56:39 +0200

    Changement du titre du site

--- src/controllers/UtilisateurController.php
+++ src/controllers/UtilisateurController.php
@@ -5,7 +5,7 @@ require_once 'src/models/Utilisateur.php';
 
 $utilisateur = new Utilisateur($pdo);
 
-// Exemple de création d'utilisateur
+// Création d'utilisateur
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $nom = $_POST['nom'];
     $email = $_POST['email'];
@@ -13,7 +13,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $utilisateur->create($nom, $email, $mot_de_passe);
 }
 
-// Exemple d'affichage de tous les utilisateurs
+// Affichage de tous les utilisateurs
 $utilisateurs = $utilisateur->getAll();
 foreach ($utilisateurs as $user) {
     echo $user['nom'] . ' - ' . $user['email'] . '<br>';
