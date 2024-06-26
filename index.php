8cc1dd27188e42a137161bc4cfcdb48169b58f6d
lucy-madec <144667458+lucy-madec@users.noreply.github.com>
2024-06-26 14:05:31 +0200

    Ajout de la favicon

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="public/css/style.css">
     <title>PixelPlush</title>
+    <link rel="icon" href="public/images/logo.png"/>
 </head>
 <header>
 
