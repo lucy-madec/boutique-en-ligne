ca4754b7272afbd34975544ba6bf15dc423e6be0
lucy-madec <144667458+lucy-madec@users.noreply.github.com>
2024-06-27 14:49:49 +0200

    Ajout des 18 autres produits dans phpmyadmin

--- config/sql_command.sql
+++ config/sql_command.sql
@@ -94,41 +94,41 @@
 --     ),
 --     (NULL, 'mario', '1');
 
--- DC
-INSERT INTO produit (id, nom, description, prix, sous_categorie_id, phare, date_ajout) VALUES 
-(1, 'batman', 'Batman, le Chevalier Noir de Gotham, prêt à combattre le crime et protéger la ville.', 20, 1, 0, CURRENT_TIMESTAMP),
-(2, 'catwoman', 'Catwoman, l\'énigmatique voleuse avec un cœur tendre, prête à voler votre cœur.', 18, 1, 0, CURRENT_TIMESTAMP),
-(3, 'superman', 'Superman, l\'Homme d\'Acier venu de Krypton, toujours prêt à sauver la planète.', 22, 1, 0, CURRENT_TIMESTAMP);
+-- -- DC
+-- INSERT INTO produit (id, nom, description, prix, sous_categorie_id, phare, date_ajout) VALUES 
+-- (1, 'batman', 'Batman, le Chevalier Noir de Gotham, prêt à combattre le crime et protéger la ville.', 20, 1, 0, CURRENT_TIMESTAMP),
+-- (2, 'catwoman', 'Catwoman, l\'énigmatique voleuse avec un cœur tendre, prête à voler votre cœur.', 18, 1, 0, CURRENT_TIMESTAMP),
+-- (3, 'superman', 'Superman, l\'Homme d\'Acier venu de Krypton, toujours prêt à sauver la planète.', 22, 1, 0, CURRENT_TIMESTAMP);
 
--- Death Note
-INSERT INTO produit (id, nom, description, prix, sous_categorie_id, phare, date_ajout) VALUES 
-(4, 'l', 'L, le détective énigmatique et génial, toujours à la recherche de la justice.', 15, 2, 0, CURRENT_TIMESTAMP),
-(5, 'light_yagami', 'Light Yagami, l\'étudiant brillant avec une ambition dangereuse.', 15, 2, 0, CURRENT_TIMESTAMP),
-(6, 'ryuk', 'Ryuk, le shinigami facétieux qui adore les pommes et les drames humains.', 15, 2, 0, CURRENT_TIMESTAMP);
+-- -- Death Note
+-- INSERT INTO produit (id, nom, description, prix, sous_categorie_id, phare, date_ajout) VALUES 
+-- (4, 'l', 'L, le détective énigmatique et génial, toujours à la recherche de la justice.', 15, 2, 0, CURRENT_TIMESTAMP),
+-- (5, 'light_yagami', 'Light Yagami, l\'étudiant brillant avec une ambition dangereuse.', 15, 2, 0, CURRENT_TIMESTAMP),
+-- (6, 'ryuk', 'Ryuk, le shinigami facétieux qui adore les pommes et les drames humains.', 15, 2, 0, CURRENT_TIMESTAMP);
 
--- Marvel
-INSERT INTO produit (id, nom, description, prix, sous_categorie_id, phare, date_ajout) VALUES 
-(7, 'black_widow', 'Black Widow, l\'espionne extraordinaire et Avenger redoutable.', 20, 3, 0, CURRENT_TIMESTAMP),
-(8, 'iron_man', 'Iron Man, le génie milliardaire avec une armure indestructible.', 25, 3, 0, CURRENT_TIMESTAMP),
-(9, 'spider_man', 'Spider-Man, le héros arachnéen qui balance de toile en toile pour sauver la ville.', 22, 3, 0, CURRENT_TIMESTAMP);
+-- -- Marvel
+-- INSERT INTO produit (id, nom, description, prix, sous_categorie_id, phare, date_ajout) VALUES 
+-- (7, 'black_widow', 'Black Widow, l\'espionne extraordinaire et Avenger redoutable.', 20, 3, 0, CURRENT_TIMESTAMP),
+-- (8, 'iron_man', 'Iron Man, le génie milliardaire avec une armure indestructible.', 25, 3, 0, CURRENT_TIMESTAMP),
+-- (9, 'spider_man', 'Spider-Man, le héros arachnéen qui balance de toile en toile pour sauver la ville.', 22, 3, 0, CURRENT_TIMESTAMP);
 
--- Stranger Things
-INSERT INTO produit (id, nom, description, prix, sous_categorie_id, phare, date_ajout) VALUES 
-(10, 'demogorgon', 'Demogorgon, la créature terrifiante du Monde à l\'Envers.', 18, 4, 0, CURRENT_TIMESTAMP),
-(11, 'eleven', 'Eleven, la fille aux pouvoirs télékinésiques et au courage immense.', 20, 4, 0, CURRENT_TIMESTAMP),
-(12, 'lucas', 'Lucas, l\'aventurier fidèle et courageux du groupe d\'amis.', 18, 4, 0, CURRENT_TIMESTAMP);
+-- -- Stranger Things
+-- INSERT INTO produit (id, nom, description, prix, sous_categorie_id, phare, date_ajout) VALUES 
+-- (10, 'demogorgon', 'Demogorgon, la créature terrifiante du Monde à l\'Envers.', 18, 4, 0, CURRENT_TIMESTAMP),
+-- (11, 'eleven', 'Eleven, la fille aux pouvoirs télékinésiques et au courage immense.', 20, 4, 0, CURRENT_TIMESTAMP),
+-- (12, 'lucas', 'Lucas, l\'aventurier fidèle et courageux du groupe d\'amis.', 18, 4, 0, CURRENT_TIMESTAMP);
 
--- Minecraft
-INSERT INTO produit (id, nom, description, prix, sous_categorie_id, phare, date_ajout) VALUES 
-(13, 'creeper', 'Creeper, la créature verte qui explose avec un sourire sinistre.', 15, 5, 0, CURRENT_TIMESTAMP),
-(14, 'ender_dragon', 'Ender Dragon, le boss ultime et majestueux du monde Minecraft.', 25, 5, 0, CURRENT_TIMESTAMP),
-(15, 'renard', 'Renard, l\'adorable créature qui rôde dans les biomes enneigés.', 12, 5, 0, CURRENT_TIMESTAMP);
+-- -- Minecraft
+-- INSERT INTO produit (id, nom, description, prix, sous_categorie_id, phare, date_ajout) VALUES 
+-- (13, 'creeper', 'Creeper, la créature verte qui explose avec un sourire sinistre.', 15, 5, 0, CURRENT_TIMESTAMP),
+-- (14, 'ender_dragon', 'Ender Dragon, le boss ultime et majestueux du monde Minecraft.', 25, 5, 0, CURRENT_TIMESTAMP),
+-- (15, 'renard', 'Renard, l\'adorable créature qui rôde dans les biomes enneigés.', 12, 5, 0, CURRENT_TIMESTAMP);
 
--- Pokémon
-INSERT INTO produit (id, nom, description, prix, sous_categorie_id, phare, date_ajout) VALUES 
-(16, 'capumain', 'Capumain, le Pokémon singe malicieux avec une queue en forme de main.', 10, 6, 0, CURRENT_TIMESTAMP),
-(17, 'pikachu', 'Pikachu, le célèbre Pokémon électrique et mascotte adorée.', 15, 6, 0, CURRENT_TIMESTAMP),
-(18, 'salameche', 'Salameche, le Pokémon de type feu avec une flamme sur sa queue.', 15, 6, 0, CURRENT_TIMESTAMP);
+-- -- Pokémon
+-- INSERT INTO produit (id, nom, description, prix, sous_categorie_id, phare, date_ajout) VALUES 
+-- (16, 'capumain', 'Capumain, le Pokémon singe malicieux avec une queue en forme de main.', 10, 6, 0, CURRENT_TIMESTAMP),
+-- (17, 'pikachu', 'Pikachu, le célèbre Pokémon électrique et mascotte adorée.', 15, 6, 0, CURRENT_TIMESTAMP),
+-- (18, 'salameche', 'Salameche, le Pokémon de type feu avec une flamme sur sa queue.', 15, 6, 0, CURRENT_TIMESTAMP);
 
 
 INSERT INTO `produit` (`id`, `nom`, `description`, `prix`, `sous_categorie_id`, `phare`, `date_ajout`) VALUES
