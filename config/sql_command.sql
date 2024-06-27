cbdc2e5b8fd4d49a01305152b32aa99c86fc45b5
lucy-madec <144667458+lucy-madec@users.noreply.github.com>
2024-06-27 15:08:08 +0200

    Ajout d'un utilisateur et d'un administrateur

--- config/sql_command.sql
+++ config/sql_command.sql
@@ -131,36 +131,7 @@
 -- (18, 'salameche', 'Salameche, le Pokémon de type feu avec une flamme sur sa queue.', 15, 6, 0, CURRENT_TIMESTAMP);
 
 
-INSERT INTO `produit` (`id`, `nom`, `description`, `prix`, `sous_categorie_id`, `phare`, `date_ajout`) VALUES
-(NULL, 'Dégout (de Vice-Versa)', 'Le personnage Dégout de Vice-Versa, toujours là pour vous rappeler les moments désagréables avec humour.', '15', '7', '0', CURRENT_TIMESTAMP),
-(NULL, 'Simba', 'Simba, le roi lion, prêt pour de nouvelles aventures avec ses amis.', '20', '7', '0', CURRENT_TIMESTAMP),
-(NULL, 'Stitch', 'Stitch, le charmant extraterrestre de Lilo et Stitch, prêt pour des câlins et des bêtises.', '18', '7', '0', CURRENT_TIMESTAMP),
-(NULL, 'Tristesse (de Vice-Versa)', 'Le personnage Tristesse de Vice-Versa, toujours là pour vous rappeler que c\'est bien d\'exprimer ses émotions.', '15', '7', '0', CURRENT_TIMESTAMP);
-
-INSERT INTO `produit` (`id`, `nom`, `description`, `prix`, `sous_categorie_id`, `phare`, `date_ajout`) VALUES
-(NULL, 'Harry Potter', 'Harry Potter, le célèbre sorcier avec sa baguette magique.', '25', '10', '0', CURRENT_TIMESTAMP),
-(NULL, 'Hermione Granger', 'Hermione Granger, l\'amie intelligente et courageuse de Harry Potter.', '25', '10', '0', CURRENT_TIMESTAMP),
-(NULL, 'Touffu', 'Touffu, le chien à trois têtes qui garde l\'entrée de la chambre des secrets.', '30', '10', '0', CURRENT_TIMESTAMP);
-
-INSERT INTO `produit` (`id`, `nom`, `description`, `prix`, `sous_categorie_id`, `phare`, `date_ajout`) VALUES
-(NULL, 'Deku', 'Deku, le héros principal de My Hero Academia avec sa détermination sans faille.', '20', '9', '0', CURRENT_TIMESTAMP),
-(NULL, 'Bakugo', 'Bakugo, le rival explosif de Deku.', '20', '9', '0', CURRENT_TIMESTAMP),
-(NULL, 'Shoto', 'Shoto, le héros aux pouvoirs de glace et de feu.', '20', '9', '0', CURRENT_TIMESTAMP);
-
-INSERT INTO `produit` (`id`, `nom`, `description`, `prix`, `sous_categorie_id`, `phare`, `date_ajout`) VALUES
-(NULL, 'Tanjiro Kamado', 'Tanjiro Kamado, le pourfendeur de démons déterminé à sauver sa sœur.', '22', '8', '0', CURRENT_TIMESTAMP),
-(NULL, 'Zenitsu Agatsuma', 'Zenitsu Agatsuma, le pourfendeur de démons aux techniques de foudre.', '22', '8', '0', CURRENT_TIMESTAMP),
-(NULL, 'Muzan Kibutsuji', 'Muzan Kibutsuji, le puissant et terrifiant roi des démons.', '22', '8', '0', CURRENT_TIMESTAMP);
-
-INSERT INTO `produit` (`id`, `nom`, `description`, `prix`, `sous_categorie_id`, `phare`, `date_ajout`) VALUES
-(NULL, 'Carapateur', 'Le Carapateur, une créature emblématique de la rivière de League of Legends.', '15', '11', '0', CURRENT_TIMESTAMP),
-(NULL, 'Gnar', 'Gnar, le petit yordle qui se transforme en un puissant monstre.', '20', '11', '0', CURRENT_TIMESTAMP),
-(NULL, 'Poro', 'Poro, l\'adorable créature à collectionner de League of Legends.', '10', '11', '0', CURRENT_TIMESTAMP),
-(NULL, 'Tibbers', 'Tibbers, l\'ours en peluche d\'Annie qui devient une bête redoutable.', '25', '11', '0', CURRENT_TIMESTAMP);
-
-INSERT INTO `produit` (`id`, `nom`, `description`, `prix`, `sous_categorie_id`, `phare`, `date_ajout`) VALUES
-(NULL, 'Boo', 'Boo, le fantôme espiègle des jeux Mario.', '12', '12', '0', CURRENT_TIMESTAMP),
-(NULL, 'Bowser', 'Bowser, le grand méchant des jeux Mario.', '20', '12', '0', CURRENT_TIMESTAMP),
-(NULL, 'Champignon', 'Le Champignon, emblème des jeux Mario qui donne des pouvoirs spéciaux.', '10', '12', '0', CURRENT_TIMESTAMP),
-(NULL, 'Luigi', 'Luigi, le frère de Mario toujours prêt pour l\'aventure.', '15', '12', '0', CURRENT_TIMESTAMP),
-(NULL, 'Mario', 'Mario, le célèbre plombier de Nintendo.', '15', '12', '0', CURRENT_TIMESTAMP);
+INSERT INTO `utilisateur` (`id`, `nom`, `email`, `mot_de passe`, `date_inscription`, `admin`) VALUES
+(1, 'lucy', 'lucy.admin@pixelplush.fr', 'Azerty123*', '2024-06-27 13:06:37', 1),
+(2, 'lucy', 'lucy@laplateforme.io', 'Azerty123*', '2024-06-27 13:06:37', 0);
+COMMIT;
