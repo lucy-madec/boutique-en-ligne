fc2cde7365012dd8fc09aa50ae034322e7740cbb
lucas-iribaren <lucas.iribaren@laplateforme.io>
2024-07-01 19:34:33 +0200

    [modification]([filtrage]): modification du controller

--- src/controllers/ProduitController.php
+++ src/controllers/ProduitController.php
@@ -1,15 +1,49 @@
-// src/controllers/ProduitController.php
 <?php
-require_once 'config/db.php';
-require_once 'src/models/Produits.php';
 
-$produitsModel = new Produit($pdo);
+require_once '../../config/db.php'; // Chemin vers le fichier de configuration de la base de données
+require_once '../models/Produit.php'; // Chemin vers le modèle Produit.php, ou Product.php maintenant
 
-$produitsPhares = $produitsModel->getProduitsPhares();
+class ProduitController {
+    private $productModel;
 
-$derniersProduits = $produitsModel->getDerniersProduits();
+    public function __construct($pdo) {
+        $this->productModel = new Product($pdo);
+    }
 
-$query = 'Peluche Mario';
-$resultatsRecherche = $produitsModel->rechercheProduits($query);
+    public function getFeaturedProducts() {
+        return $this->productModel->getFeaturedProducts();
+    }
 
-require 'src/views/produits.php';
+    public function getLatestProducts() {
+        return $this->productModel->getLatestProducts();
+    }
+
+    public function searchProducts($query) {
+        return $this->productModel->searchProducts($query);
+    }
+
+    public function getSubcategories($category_id) {
+        return $this->productModel->getSubcategories($category_id);
+    }
+
+    public function getProductsBySubcategory($subcategory_id) {
+        return $this->productModel->getProductsBySubcategory($subcategory_id);
+    }
+
+    public function addProduct($name, $description, $price, $subcategory_id, $featured = 0) {
+        return $this->productModel->addProduct($name, $description, $price, $subcategory_id, $featured);
+    }
+
+    public function updateProduct($id, $name, $description, $price, $subcategory_id, $featured = 0) {
+        return $this->productModel->updateProduct($id, $name, $description, $price, $subcategory_id, $featured);
+    }
+
+    public function deleteProduct($id) {
+        return $this->productModel->deleteProduct($id);
+    }
+
+}
+
+// Instanciation du contrôleur avec la connexion PDO
+$controller = new ProduitController($pdo);
+?>
