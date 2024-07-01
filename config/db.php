56fd21c890714cfe81c90e2672c4fafbcc8bef99
lucas-iribaren <lucas.iribaren@laplateforme.io>
2024-07-01 18:59:05 +0200

    [modification]([db]): supression d'une partie inutile

--- config/db.php
+++ config/db.php
@@ -1,20 +1,12 @@
-// config/db.php
 <?php
 $host = '127.0.0.1';
 $db = 'pixel_plush';
 $user = 'root';
 $pass = '';
-$charset = 'utf8mb4';
-
-$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
-$options = [
-    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    PDO::ATTR_EMULATE_PREPARES   => false,
-];
 
 try {
-    $pdo = new PDO($dsn, $user, $pass, $options);
-} catch (\PDOException $e) {
-    throw new \PDOException($e->getMessage(), (int)$e->getCode());
+    $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8mb4", $user, $pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo 'La connexion a échoué : ' . $e->getMessage();
 }
